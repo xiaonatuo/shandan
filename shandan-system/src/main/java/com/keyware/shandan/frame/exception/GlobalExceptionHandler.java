153fd20cb38ccfb1a4b692374d878b6a07770dcf
xiaonatuo <371864209@qq.com>
2021-06-17 13:37:19 +0800

    综合浏览整合编目功能模块

--- shandan-system/src/main/java/com/keyware/shandan/frame/exception/GlobalExceptionHandler.java
+++ shandan-system/src/main/java/com/keyware/shandan/frame/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.keyware.shandan.frame.exception;
 import com.keyware.shandan.common.entity.Result;
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -20,14 +22,13 @@ import org.springframework.web.multipart.MultipartException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @Value("${spring.servlet.multipart.max-file-size}")
-    private String maxFileSize;
-
-    @Value("spring.servlet.multipart.max-request-size")
-    private String maxRequestSize;
+    @Autowired
+    private Environment env;
 
     @ExceptionHandler(MultipartException.class)
     public Result<Object> exception(MaxUploadSizeExceededException e){
+        String maxFileSize = env.getProperty("spring.servlet.multipart.max-file-size");
+        String maxRequestSize = env.getProperty("spring.servlet.multipart.max-request-size");
         if (e.getCause().getCause() instanceof FileSizeLimitExceededException){//单个文件大小超出限制抛出的异常
             return Result.of(null, false, "单个上传文件大小不能超过" + maxFileSize);
         }else if (e.getCause().getCause() instanceof SizeLimitExceededException){//总文件大小超出限制抛出的异常
