d0d4979ba5499f7fe502236273899caf4782a822
xiaonatuo <371864209@qq.com>
2021-06-28 14:02:16 +0800

    检索页面条件检索框架

--- shandan-browser/src/main/resources/static/js/index.js
+++ shandan-browser/src/main/resources/static/js/index.js
@@ -6,12 +6,14 @@
  * @author Administrator
  * @since 2021/6/17
  */
-layui.use(['layer', 'globalTree', 'dropdown'], function () {
+layui.use(['layer', 'globalTree', 'form', 'element', 'laydate'], function () {
     const tree = layui.globalTree,
         layer = layui.layer,
-        dropdown = layui.dropdown;
+        form = layui.form,
+        element = layui.element,
+        laydate = layui.laydate;
 
-    // initConditionBlock()
+    initConditionBlock()
     initDirTree();
 
     /**
@@ -24,35 +26,94 @@ layui.use(['layer', 'globalTree', 'dropdown'], function () {
             data: [{id: '-', parentId: '', title: '根目录', leaf: false, last: false, spread: false}],
             cache: false,
             done: function (nodes, elem) {
-                console.info(nodes);
                 // 模拟鼠标点击事件展开第一层目录
                 $('i.dtree-icon-jia[data-id="-"]').click();
             },
         });
     }
 
+    /**
+     * 搜索条件模块初始化
+     */
     function initConditionBlock(){
-        dropdown.render({
-            elem: '#condition-btn',
-            align: 'right',
-            content: ['<div class="layui-tab layui-tab-brief">'
-                ,'<ul class="layui-tab-title">'
-                ,'<li class="layui-this">Tab header 1</li>'
-                ,'<li>Tab header 2</li>'
-                ,'<li>Tab header 3</li>'
-                ,'</ul>'
-                ,'<div class="layui-tab-content">'
-                ,'<div class="layui-tab-item layui-text layui-show"><p style="padding-bottom: 10px;">在 content 参数中插入任意的 html 内容，可替代默认的下拉菜单。 从而实现更多有趣的弹出内容。</p><p> 是否发现，dropdown 组件不仅仅只是一个下拉菜单或者右键菜单，它能被赋予许多的想象可能。</p></div>'
-                ,'<div class="layui-tab-item">Tab body 2</div>'
-                ,'<div class="layui-tab-item">Tab body 3</div>'
-                ,'</div>'
-                ,'</div>'].join('')
-            ,style: 'width: 1542px; height: 200px; padding: 0 15px; box-shadow: 1px 1px 30px rgb(0 0 0 / 12%);'
-            ,ready: function(){
-                layui.use('element', function(element){
-                    element.render('tab');
-                });
-            }
+        // 监听更多条件按钮点击事件
+        $('#condition-btn').click(function () {
+            $('#condition-div').slideToggle('fast', 'linear', conditionSlideToggle)
         });
+
+        // 监听标签点击事件
+        element.on('tab(condition-tab)', function(data){
+            // 点击后移除选中样式
+            $('#condition-tab ul li').removeClass('layui-this')
+        });
+
+        // 监听条件标签删除事件
+        element.on('tabDelete(condition-tab)', function(data){
+            let key = $(this).parent().data('key');
+            let formVal = form.val('search-form');
+            formVal[key] = '';
+            form.val('search-form', formVal);
+            beginSearch();
+        });
+
+        // 搜索按钮点击事件监听
+        $('#begin-search-btn').on('click', beginSearch);
+
+        // 监听重置按钮
+        $('#condition-clear-btn').on('click', function(){
+            let formVal = form.val('search-form') || {};
+            for(let key in formVal){
+                formVal[key] = '';
+            }
+            form.val('search-form', formVal);
+            renderConditionTabByForm();
+        });
+
+        //日期选择器
+        laydate.render({
+            elem: '#input-date',
+            format: "yyyy-MM-dd"
+        });
+    }
+
+    /**
+     * 条件下拉框显示隐藏的回调
+     */
+    function conditionSlideToggle(){
+        const display = $('#condition-div').css('display');
+        // 隐藏时，根据搜索表单的值渲染条件标签，并根据条件检索
+        if(display === 'none'){
+            renderConditionTabByForm();
+        }
+    }
+
+    /**
+     * 根据条件表单数据渲染条件标签
+     */
+    function renderConditionTabByForm(){
+        const formVal = form.val('search-form') || {}
+        let htm = '';
+        for (let key in formVal) {
+            let val = formVal[key];
+            if(val.trim()){
+                let title = $(`#condition-div .layui-form-item input[name='${key}']`).parent().prev().text();
+                htm += `<li data-key="${key}"><label style="color:gray">${title}</label>: ${val}</li>`
+            }
+        }
+        $('#condition-tab ul').html(htm);
+        element.render('tab')
+        // 开始检索
+        beginSearch();
+    }
+
+    /**
+     * 开始检索
+     */
+    function beginSearch(){
+        console.info('开始条件查询数据:', form.val('search-form'))
+        // 如果搜索条件下拉框处于显示状态则隐藏
+        if($('#condition-div').css('display') !== 'none'){
+            $('#condition-div').slideToggle('fast');
+        }
     }
 });
