ff3755098600a76a305652fdb2f67b57f3144c91
xiaonatuo <371864209@qq.com>
2021-07-07 09:35:45 +0800

    ES存储公共实体类

---
+++ shandan-system/src/main/java/com/keyware/shandan/system/entity/EsCommonEntity.java
@@ -0,0 +1,132 @@
+package com.keyware.shandan.system.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.keyware.shandan.common.entity.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * ES公共字段实体类
+ *
+ * @author Administrator
+ * @since 2021/7/6
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Document(indexName = "shandan")
+public class EsCommonEntity extends BaseEntity implements Serializable {
+    private static final long serialVersionUID = -3633405631787992830L;
+
+    /**
+     * 主键ID
+     */
+    @TableId("ID")
+    @Id
+    private String id;
+
+    /**
+     * 录入人员
+     */
+    @Field
+    @TableField("ENTRYSTAFF")
+    private String entryStaff;
+
+    /**
+     * 装备型号
+     */
+    @Field
+    @TableField("EQUIPMENTMODEL")
+    private String equipmentModel;
+
+    /**
+     * 收文时间
+     */
+    @Field
+    @TableField("INPUTDATE")
+    private Date inputDate;
+
+    /**
+     * 文件来源
+     */
+    @Field
+    @TableField("SOURCE")
+    private String source;
+
+    /**
+     * 任务代号
+     */
+    @Field
+    @TableField("TASKCODE")
+    private String taskCode;
+
+    /**
+     * 任务性质
+     */
+    @Field
+    @TableField("TASKNATURE")
+    private String taskNature;
+
+    /**
+     * 部队代号
+     */
+    @Field
+    @TableField("TROOPCODE")
+    private String troopCode;
+
+    /**
+     * 目标编号
+     */
+    @Field
+    @TableField("TARGETNUMBER")
+    private String targetNumber;
+
+    /**
+     * 导弹编号
+     */
+    @Field
+    @TableField("MISSILENUMBER")
+    private String missileNumber;
+
+    /**
+     * 大文本字段，用于保存文件中的文本信息
+     */
+    @Field
+    @TableField(exist = false)
+    private String text;
+
+    /**
+     * 创建用户
+     */
+    @Field
+    @TableField(value = "CREATE_USER", fill = FieldFill.INSERT)
+    private String createUser;
+
+    /**
+     * 创建时间
+     */
+    @Field
+    @TableField(value = "CREATE_TIME", fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改用户
+     */
+    @Field
+    @TableField(value = "MODIFY_USER", fill = FieldFill.INSERT_UPDATE)
+    private String modifyUser;
+
+    /**
+     * 修改时间
+     */
+    @Field
+    @TableField(value = "MODIFY_TIME", fill = FieldFill.INSERT_UPDATE)
+    private Date modifyTime;
+}
