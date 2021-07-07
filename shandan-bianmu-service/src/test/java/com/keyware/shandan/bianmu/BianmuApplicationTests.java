2050852a267ec533dde8421fd943475d7bd39593
xiaonatuo <371864209@qq.com>
2021-07-07 16:08:05 +0800

    test测试

--- shandan-bianmu-service/src/test/java/com/keyware/shandan/bianmu/BianmuApplicationTests.java
+++ shandan-bianmu-service/src/test/java/com/keyware/shandan/bianmu/BianmuApplicationTests.java
@@ -1,12 +1,28 @@
 package com.keyware.shandan.bianmu;
 
-import org.junit.jupiter.api.Test;
+import com.alibaba.fastjson.JSONObject;
+import com.keyware.shandan.bianmu.es.repository.EsSysFileRepository;
+import com.keyware.shandan.system.entity.SysFile;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class BianmuApplicationTests {
 
-    @Test
+    @Autowired
+    private EsSysFileRepository repository;
+
+    //@Test
     public void contextLoads() {
+        SysFile file = new SysFile();
+        file.setEntityId("1234");
+        file.setFileName("1234.txt");
+        file.setFileType("txt");
+        SysFile count = repository.save(file);
+        System.out.println(JSONObject.toJSON(count));
     }
 }
