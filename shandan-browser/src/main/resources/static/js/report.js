f0ef86e7d5ef44d83e8268c165cf222e31c1b5fb
xiaonatuo <371864209@qq.com>
2021-07-14 11:13:07 +0800

    统计报表前端代码

--- shandan-browser/src/main/resources/static/js/report.js
+++ shandan-browser/src/main/resources/static/js/report.js
@@ -1,211 +1,261 @@
 /**
- *  统计报表js组件
+ *  统计报表组件
  *
  * @author GuoXin
- * @since 2021/7/1
+ * @since 2021/7/12
  */
-// 统计报表对象
-class ReportItem{
-    title = '';
-    type = '';
-    data = {};
-    condition = {};
-    xField = '';
-    yField = ''
-    echarts = {};
-    constructor(options) {
-        options = options || {};
-        this.type = options.type || '';
-        this.data = options.data || '';
-        this.condition = options.condition || {};
-    }
-}
 function ReportComponent(layer, form) {
-    this.template = {
-        main: `
-            <div id="layer-content-report" >
-              <div class="report-main">
-                  <div class="report-btn-line">
-                      <a class="layui-btn layui-btn-sm layui-btn-normal" id="btn-add"><i class="layui-icon layui-icon-add-1"></i>定义新报表</a>
-                      <!--<a class="layui-btn layui-btn-sm layui-btn-normal" id="btn-download"><i class="layui-icon layui-icon-download-circle"></i>下载报表</a>-->
-                  </div>
-                  <div class="report-items" id="report-items">
-                  </div>
-              </div>
-            </div>`,
-        chooseType: `
-        <div class="layui-form" lay-filter="form-report-select" id="form-report-select">
-            <div class="layui-form-item" style="margin: 0; padding:20px;">
-                <select name="reportType">
-                    <option value="pie" selected>饼图</option>
-                    <option value="bar">柱状图</option>
-                    <option value="line">折线图</option>
-                </select>
-            </div>
-        </div>`,
-        configPie: `
-        <div class="layui-form" lay-filter="form-config-pie" id="form-config-pie">
-            <div class="layui-form-item" style="padding:0 20px;">
-                <div class="layui-form-label" style="width: 100px">标题</div>
-                <div class="layui-input-block" style="margin-left: 140px;">
-                    <input type="text" class="layui-input" placeholder="设置图表的标题">
-                </div>
-            </div>
-            <div class="layui-form-item" style="padding:0 20px;">
-                <div class="layui-form-label" style="width: 100px">统计维度</div>
-                <div class="layui-input-block" style="margin-left: 140px;">
-                    <select name="field" id="selectField" lay-search>
-                        <option value="source" selected>文件来源</option>
-                        <option value="entryStaff">录入人员</option>
-                        <option value="taskNature">任务性质</option>
-                        <option value="taskCode">任务代号</option>
-                        <option value="targetNumber">目标编号</option>
-                        <option value="troopCode">部队代号</option>
-                        <option value="equipmentModel">装备型号</option>
-                        <option value="missileNumber">导弹编号</option>
-                    </select>
-                </div>
-            </div>
-        </div>`,
-        configBar: ``,
-        configLine: ``
-    };
-    this.layer = layer || {};
-    this.form = form || {};
-    this.formData = {};
-    this.conditions = [];
-    this.reports = [];
+    this.layer = layer || {}; // layui layer组件
+    this.form = form || {}; // layui form组件
+    this.formData = {}; // 检索页面的form表单数据
+    this.fields = common_fields; // 当前字段集合，默认只有公共字段
+    this.size = 0;
 }
 
 /**
- * 打开统计报表主要窗口
+ * 打开统计报表主窗口
  */
-ReportComponent.prototype.openMainLayer = function(){
-    // 条件表单数据
+ReportComponent.prototype.openMainLayer = function () {
     const _this = this;
     layer.open({
-        //id: 'layer-content-report',
         type: 1,
         title: '统计报表',
         area: ['800px', '600px'],
-        content: this.template.main,
+        content: template_main,
         success: function (layero, index) {
+            // 打开即页面全屏
             layer.full(index);
 
             // 新增报表，先选择图表类型，然后再选择需要的字段
             $('#btn-add').on('click', function () {
-                _this.openEchartsChooseLayer();
+                _this.openEchartsConfigLayer();
             });
         }
     })
 }
 
 /**
- * 打开echarts类型选择框
+ * 打开图表配置窗口
  */
-ReportComponent.prototype.openEchartsChooseLayer = function () {
-
+ReportComponent.prototype.openEchartsConfigLayer = function () {
     const _this = this;
     layer.open({
-        title: '请选择图表类型',
-        type:1,
+        title: '统计报表配置',
+        type: 1,
         resize: false,
         closeBtn: false,
-        area:['300px', '180px'],
-        btn: ['确定','取消'],
-        content: _this.template.chooseType,
-        success: function(layerObj, index){
-            _this.form.render('select');
-            $('#form-report-select').parent().css('overflow','visible')
+        btn: ['确定', '取消'],
+        content: template_config,
+        success: function (layerObj, index) {
+            _this.renderSelect(common_fields);
+            $('#echartsConfigForm').parent().css('overflow', 'visible')
         },
-        yes: function(index){
-            const selectForm = _this.form.val('form-report-select');
-            let report = new ReportItem({type: selectForm['reportType']});
-            _this.openConfigLayer(report)
-            layer.close(index);
+        yes: function (index) {
+            const formVal = _this.form.val('echartsConfigForm');
+            if(_this.validate(formVal)){
+                _this.requestData(formVal);
+                layer.close(index);
+            }
         }
     });
-};
+}
 
 /**
- * 打开echarts配置窗口
- * @param report
+ * 初始化字段下拉框数据
+ * @param metadataId 元数据ID
  */
-ReportComponent.prototype.openConfigLayer = function(report){
+ReportComponent.prototype.initFieldSelect = function (metadataId) {
     const _this = this;
-    layer.open({
-        title: '统计报表参数配置',
-        type:1,
-        resize: false,
-        closeBtn: false,
-        area:['400px', '300px'],
-        btn: ['确定','取消'],
-        content: _this.template.configPie,
-        success: function(layerObj, index){
-            // 判断是否元数据表，如果是，则查询所有字段配置，并添加到字段下拉框
-            let formData = _this.formData
-            if(formData.metadataId){
-                $.get(`${ctx}/report/metadata/columns/${formData.metadataId}`,{}, function(res){
-                    if(res.flag && res.data){
-                        let cols = JSON.parse(res.data);
-                        for (let item of cols) {
-                            if(item.columnName.toLowerCase().endsWith('id')){continue}
-                            let text = item.comment || item.columnName;
-                            let value = item.columnName;
-                            $('#selectField').append(`<option value="${value}">${text}</option>`);
-                        }
+    $.get(`${ctx}/report/metadata/columns/${metadataId}`, {}, function (res) {
+        let fields = $.extend([], common_fields);
+        if (res.flag && res.data) {
+            let cols = JSON.parse(res.data);
+            for (let item of cols) {
+                const field = item.columnName, type = item.type, comment = item.comment;
+                if (!field || field.toLowerCase().endsWith('id')) {
+                    continue
+                }
+                let flag = true;
+                // 将现有字段集合中同名但大小写不同的字段替换掉
+                for (let i = 0; i < fields.length; i++) {
+                    if (fields[i].field.toUpperCase() == field.toUpperCase()) {
+                        fields[i].field = field;
+                        fields[i].type = type;
+                        fields[i].comment = comment;
+                        flag = false;
+                        break;
                     }
-                    _this.form.render('select');
-                })
-            }else{
-                _this.form.render('select');
+                }
+                flag && fields.push({field, type, comment});
             }
-            $('#form-config-pie').parent().css('overflow','visible')
-        },
-        yes: function(index){
-            const selectForm = _this.form.val('form-config-pie');
-            if(report.type == 'pie'){
-                report.xField = selectForm.field;
-            }
-            _this.requestData(report);
-            layer.close(index);
+            _this.fields = fields;
         }
-    });
+        _this.renderSelect();
+    })
+}
+/**
+ * 渲染字段下拉框
+ */
+ReportComponent.prototype.renderSelect = function () {
+    const _this = this;
+    let options = `<option value="">选择字段</option>`;
+    for (const field of _this.fields) {
+        const text = field.comment || field.field;
+        options += `<option value="${field.field}">${text}</option>`
+    }
+    $('#selectFieldX').html(options)
+    _this.form.render('select');
+    // 维度字段下拉框事件,根据选择字段类型判断是否显示范围输入选项
+    _this.form.on('select(selectFieldX)', function ({elem, value, othis}) {
+        const fieldType = _this.findFieldType(value);
+        _this.form.val('echartsConfigForm', {fieldXType: fieldType});
+        if (fieldType) {
+            if (dm_date_types.includes(fieldType)) {
+                $('#range-date-item').removeClass('layui-hide')
+            } else if (dm_number_types.includes(fieldType)) {
+                $('#range-number-item').removeClass('layui-hide')
+            } else {
+                $('#range-date-item').addClass('layui-hide')
+                $('#range-number-item').addClass('layui-hide')
+                _this.form.val('echartsConfigForm', {dateInterval: "", numberInterval: ""});
+            }
+        }
+    })
+    // 聚合指标下拉框事件监听
+    _this.form.on('select(aggregationType)', function ({elem, value, othis}) {
+        if (value == 'sum' || value == 'avg') {
+            // 渲染数值类型字段
+            const numberFields = _this.fields.filter(field => dm_number_types.includes(field.type));
+            let options = '<option value="">选择字段</option>'
+            if (numberFields.length == 0) {
+                options = '<option value="">没有可用字段</option>'
+            }
+            for (const field of numberFields) {
+                const text = field.comment || field.field;
+                options += `<option value="${field.field}">${text}</option>`
+            }
+            $('#selectFieldY').html(options)
+            $('#field-select-item-y').removeClass('layui-hide');
+            _this.form.render('select')
+        } else {
+            $('#field-select-item-y').addClass('layui-hide');
+            _this.form.val('echartsConfigForm', {fieldY: ""});
+        }
+    })
+
+    // 图表类型下拉框事件监听
+    _this.form.on('select(reportType)', function({elem, value, othis}){
+        // 如果选择饼图，则聚合方式只能选则计数
+        if(value == 'pie'){
+            $('#aggregationType option[value="count"]').siblings().attr('disabled', true)
+            _this.form.val('echartsConfigForm', {aggregationType: 'count'});
+        }else{
+            $('#aggregationType option[value="count"]').siblings().attr('disabled', false)
+        }
+        _this.form.render('select')
+    })
+}
+
+/**
+ * 查找字段类型
+ * @param field
+ */
+ReportComponent.prototype.findFieldType = function (field) {
+    const fields = this.fields;
+    for (let f of fields) {
+        if (field == f.field) {
+            return f.type;
+        }
+    }
+    return '';
+}
+
+/**
+ * 设置属性
+ * @param value -
+ */
+ReportComponent.prototype.setFormData = function (value) {
+    this.formData = value;
+    // 需要判断目录树是否选中了元数据，如果是，则需要查询字段，然后渲染字段下拉框
+    if (value.metadataId) {
+        this.initFieldSelect(value.metadataId);
+    } else {
+        this.renderSelect(common_fields);
+    }
+}
+
+/**
+ * 设置条件参数
+ * @param value -
+ */
+ReportComponent.prototype.setConditions = function (value) {
+    this.conditions = value;
 }
 
 /**
  * 请求数据
+ * @param formVal
  */
-ReportComponent.prototype.requestData = function(report){
+ReportComponent.prototype.requestData = function (formVal) {
     const _this = this;
-    const data = {
-        type: report.type,
-        fieldX: report.xField,
-        fieldY: report.yField,
-        conditions: _this.conditions
-    }
-    $.post(`${ctx}/report/data`, data, function(res){
-        if(res.flag){
-            report.data = res.data;
-            _this.renderEcharts(report);
+    formVal.conditions = _this.conditions
+    $.post(`${ctx}/report/data`, formVal, function (res) {
+        if (res.flag) {
+            _this.renderEcharts(res.data);
         }
     });
 }
 
 /**
- * 渲染echarts
- * @param report
+ * 数据校验
+ * @param formVal
  */
-ReportComponent.prototype.renderEcharts = function(report){
-    //console.info(report);
+ReportComponent.prototype.validate = function(formVal){
+    // 数据校验
+    if(!formVal.title || formVal.title.trim() == ''){
+        this.layer.msg('请输入标题');
+        return;
+    }
+    if(!formVal.fieldX){
+        this.layer.msg('请设置统计维度字段');
+        return;
+    }else{
+        if(dm_date_types.includes(formVal.fieldXType) && !formVal.dateInterval){
+            this.layer.msg('请设置时间间隔');
+            return;
+        }
+        if(dm_number_types.includes(formVal.fieldXType) && !formVal.numberInterval){
+            this.layer.msg('请设置范围');
+            return;
+        }
+    }
+    if(!formVal.aggregationType){
+        this.layer.msg('请设置聚合方式');
+        return;
+    }else{
+        if((formVal.aggregationType == 'sum' || formVal.aggregationType == 'avg') && !formVal.fieldY){
+            this.layer.msg('请设置聚合字段');
+            return;
+        }
+    }
+    return true;
+}
+
+/**
+ * 渲染echarts
+ * @param reportData
+ */
+ReportComponent.prototype.renderEcharts = function(reportData){
     const _this = this;
-    const elemId = `echarts-item-${_this.reports.length + 1}`;
+    const data = $.extend(true, _this.form.val('echartsConfigForm'), {data: reportData});
+    console.info(data);
+    const elemId = `echarts-item-${_this.size}`;
     $('#report-items').append(`<div class="echarts-item" id="${elemId}"></div>`)
     let chartDom = document.getElementById(elemId);
     let echartsItem = echarts.init(chartDom);
     let option = {
         title: {
-            text: report.title || '统计图',
+            text: data.title || '统计图',
             left: 'center'
         },
         toolbox: {
@@ -227,9 +277,9 @@ ReportComponent.prototype.renderEcharts = function(report){
         },
         series: [
             {
-                type: report.type,
+                type: data.reportType,
                 radius: '50%',
-                data: report.data.series,
+                data: data.data.series,
                 emphasis: {
                     itemStyle: {
                         shadowBlur: 10,
@@ -240,11 +290,11 @@ ReportComponent.prototype.renderEcharts = function(report){
             }
         ]
     };
-    if(report.type !== 'pie'){
+    if(data.reportType !== 'pie'){
         option.xAxis = [
             {
                 type: 'category',
-                data: report.data.xAxis,
+                data: data.data.xAxis,
                 axisTick: {
                     alignWithLabel: true
                 }
@@ -253,31 +303,5 @@ ReportComponent.prototype.renderEcharts = function(report){
         option.yAxis = [{type: 'value'}]
     }
     option && echartsItem.setOption(option);
-    report.echarts = echartsItem;
-    _this.addReport(report);
-}
-
-/**
- * 添加统计报表对象
- * @param report
- */
-ReportComponent.prototype.addReport = function (report) {
-    this.reports.push(report);
-};
-
-/**
- * 设置属性
- * @param key
- * @param value
- */
-ReportComponent.prototype.setFormData = function (value){
-    this.formData = value;
-}
-
-/**
- * 设置条件参数
- * @param value
- */
-ReportComponent.prototype.setConditions = function (value){
-    this.conditions = value;
+    _this.size += 1;
 }
