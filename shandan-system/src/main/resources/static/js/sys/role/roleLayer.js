1451cac6e7ebca80a12fce00339c63fe394effc9
xiaonatuo <371864209@qq.com>
2021-08-23 18:19:49 +0800

    配置用户角色保存提示

--- shandan-system/src/main/resources/static/js/sys/role/roleLayer.js
+++ shandan-system/src/main/resources/static/js/sys/role/roleLayer.js
@@ -1,12 +1,14 @@
-layui.use(['form', 'layer'], function(){
+layui.use(['form', 'layer'], function () {
     let form = layui.form;
     let layer = layui.layer;
 
     form.on("checkbox", function (elem) {
         let data = {roleId: $(elem.elem).data('roleId'), userId: $(elem.elem).data('userId')};
         $.post(`${ctx}/sys/userRole/save`, data, function (res) {
-            if(!res.flag){
+            if (!res.flag) {
                 layer.msg('保存失败')
+            } else {
+                layer.msg('保存成功')
             }
         });
     });
