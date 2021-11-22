fbbbb4e5d37715f7fcd8161747fe9cc0a953f9d6
xiaonatuo <371864209@qq.com>
2021-11-22 16:13:53 +0800

    数据字典组件优化， 增加根据html标签自动渲染 增加根据组件ID绑定onchange事件回调函数方法 增加根据组件ID设置组件显示的数据的方法 增加组件说明及示例

--- shandan-common/src/main/resources/static/js/common/layui/extend/dict.js
+++ shandan-common/src/main/resources/static/js/common/layui/extend/dict.js
@@ -1,3 +1,31 @@
+// 组件使用示例
+// 必须先声明使用该组件
+/*layui.use(['dict'], function () {
+    // 方式一：在html中添加如下标签元素
+    //<div dict-component="select" dict-type="data_type" dict-name="dataType" id="dict-data-type"></div>
+    // 属性说明：
+    // dict-component: 组件类型：select、radio、checkbox
+    // dict-type: 数据字典类型
+    // dict-name: 数据字典名称，相对于form表单来说，作用相当于input标签中的name
+    // id：组件的ID，同时该值会用于渲染之后该组件对于layui的lay-filter属性
+    // 绑定数据：layui.dict.setData(id, data)
+    // 绑定onchange事件回调函数：layui.dict.onchange(id, callback)
+
+    // 方式二：使用js初始化
+    let dict = layui.dict;
+
+    let dictOption = {
+        elem: '', //渲染到目标元素的ID选择器或者元素DOM
+        id: '', //组件的ID，同时该值会用于渲染之后该组件对于layui的lay-filter属性
+        name: '', //数据字典名称，相对于form表单来说，作用相当于input标签中的name
+        formFilter: '', //所在form表单标签上lay-filter的值
+        type: '', //数据字典类型
+        data: '', //初始选中的数据，如果type为‘checkbox’，也可以传入数组或者,拼接的包含多个值的字符串
+        onchange: '' //数据改变后的回调函数
+    };
+    dict.select(dictOption);
+});*/
+
 /**
  *  数据字典组件
  *
@@ -6,29 +34,22 @@
  */
 layui.define(['form'], function (exports) {
     let form = layui.form;
+    const _cache = new Map();
 
     /**
      * 数据字典组件对象
      */
     class DictComponent {
-        // 数据字典
-        DICT;
-        // 渲染元素的ID
-        elem = '';
-        // 组件的ID
-        id = '';
-        // 组件的name，相对于form表单来说
-        name = '';
-        // 渲染类型
-        type = '';
-        // form表单标签上lay-filter的值
-        formFilter = '';
-        // 数据字典类型
-        dictType = '';
-        // 选中的数据
-        data = '';
-        // 数据改变后的回调函数
-        change;
+
+        DICT;// 数据字典
+        elem = '';// 渲染元素的ID
+        id = '';// 组件的ID
+        name = '';// 组件的name，相对于form表单来说
+        type = '';// 渲染类型
+        formFilter = '';// form表单标签上lay-filter的值
+        dictType = '';// 数据字典类型
+        data = '';// 选中的数据
+        change;// 数据改变后的回调函数
 
         constructor(type, options) {
             if (!type) {
@@ -66,54 +87,78 @@ layui.define(['form'], function (exports) {
          * 渲染组件
          */
         render() {
-            if (!this.DICT) {
-                return
+            let dict_list;
+            if (this.DICT) {
+                dict_list = this.DICT[this.dictType] || [];
+            }else{
+                dict_list = [];
             }
-            let dict_list = this.DICT[this.dictType];
-            let _elem = $(`#${this.elem}`);
+
+            let _elem;
+            if (typeof this.elem == 'string') {
+                // 如果为string类型，则认为是ID选择器，判断是否有#，没有则加上
+                _elem = this.elem.startsWith('#') ? $(this.elem) : $(`#${this.elem}`);
+            } else {
+                // 如果不是jquery对象，则包装为jquery对象
+                _elem = this.elem instanceof jQuery ? this.elem : $(this.elem);
+            }
             _elem.addClass('layui-hide');
             let template = '';
             switch (this.type) {
                 case 'select':
                     template = selectDOM(this, dict_list);
                     break;
-                case 'checkbox':
-                    template = normalSingleNodeDOM(this, dict_list);
-                    break;
                 case 'radio':
+                case 'checkbox':
                     template = normalSingleNodeDOM(this, dict_list);
                     break;
             }
             _elem.after(template);
             form.render(this.type);
 
-            // 设置事件监听
-            form.on(`${this.type}(${this.id})`, this.change);
+            this.onchange();
 
             // 设置默认值
-            this.setFormData(this.data);
+            this.setData(this.data);
+            _cache.set(this.id, this);
         }
 
-        setFormData(data){
-            if(!this.formFilter){
+        setData(data) {
+            if (!data) return;
+            if (!this.formFilter) {
                 console.error('options中没有设置formFilter');
                 return;
             }
-            if(this.type == 'checkbox'){
+            if (typeof data == 'object') {
+                data = data[this.name] || '';
+            }
+
+            if (this.type == 'checkbox') {
                 let values = data;
                 if (!Array.isArray(data)) {
                     values = data.split(',');
                 }
-                for(let val of values){
+                for (let val of values) {
                     $(`[lay-filter="${this.formFilter}"] input[type="checkbox"][name="${this.name}"][value="${val}"]`).attr('checked', true);
                 }
-            }else{
+            } else {
                 if (Array.isArray(data)) data = data[0]
                 let formData = {};
                 formData[this.name] = data;
                 form.val(this.formFilter, formData);
             }
             form.render(this.type);
+        }
+
+        /**
+         * onchange事件回调函数绑定
+         * @param callback
+         */
+        onchange(callback) {
+            if (callback) {
+                this.change = callback;
+            }
+            form.on(`${this.type}(${this.id})`, this.change);
         }
     }
 
@@ -125,7 +170,7 @@ layui.define(['form'], function (exports) {
      * @returns {string}
      */
     function selectDOM(_this, list) {
-        let nodes = [];
+        let nodes = [`<option value="">请选择</option>`];
         for (let dict of list) {
             nodes.push(`<option value="${dict.dictCode}">${dict.dictName}</option>`)
         }
@@ -147,14 +192,48 @@ layui.define(['form'], function (exports) {
         return nodes.join('');
     }
 
+    /**
+     * 自动渲染
+     */
+    $(function () {
+        let elements = $(`[dict-component]`);
+        elements.each(function (index) {
+            let _elem = $(this);
+            let elem = this,
+                id = _elem.attr('id'),
+                name = _elem.attr('dict-name'),
+                type = _elem.attr('dict-type'),
+                dictType = _elem.attr('dict-component'),
+                formFilter = getFormElementFilter(_elem);
+
+            let option = {elem, id, name, type, formFilter};
+            new DictComponent(dictType, option)
+        });
+    });
+
+    /**
+     * 获取元素所在form表单的layui过滤器名称
+     * @param $elem
+     * @returns {string|*|jQuery}
+     */
+    function getFormElementFilter($elem) {
+        for (let index = 0; index < $elem.parents().length; index++) {
+            let elem = $elem.parents()[index];
+            if (elem.tagName === 'FORM' || $(elem).hasClass('layui-form')) {
+                return $(elem).attr('lay-filter');
+            }
+        }
+        return '';
+    }
+
 
     /**
      * 暴露组件
      * options：{
-     *     elem: '', 渲染到目标元素的ID
-     *     id: '', 组件的ID
+     *     elem: '', 渲染到目标元素的ID选择器或者元素DOM
+     *     id: '', 组件的ID，同时该值会用于渲染之后该组件对于layui的lay-filter属性
      *     name: '', 组件的name，相对于form表单来说
-     *     formFilter: '', form表单标签上lay-filter的值
+     *     formFilter: '', 所在form表单标签上lay-filter的值
      *     type: '', 数据字典类型
      *     data: '', 初始选中的数据，如果type为‘checkbox’，也可以传入数组或者,拼接的包含多个值的字符串
      *     onchange:'' 数据改变后的回调函数
@@ -162,15 +241,61 @@ layui.define(['form'], function (exports) {
      * @type {{select: (function(*=): DictComponent), checkbox: (function(*=): DictComponent), radio: (function(*=): DictComponent)}}
      */
     const component = {
+        /**
+         * 渲染下拉选择框组件
+         * @param options
+         * @returns {DictComponent}
+         */
         select: function (options) {
             return new DictComponent('select', options);
         },
+
+        /**
+         * 渲染单选按钮组件
+         * @param options
+         * @returns {DictComponent}
+         */
         radio: function (options) {
             return new DictComponent('radio', options);
         },
+
+        /**
+         * 渲染复选框组件
+         * @param options
+         * @returns {DictComponent}
+         */
         checkbox: function (options) {
             return new DictComponent('checkbox', options);
+        },
+
+        /**
+         * 为组件绑定onchange事件的回调函数
+         * @param id 组件的ID
+         * @param callback onchange事件的回调函数
+         */
+        onchange: function (id, callback) {
+            let dict = _cache.get(id);
+            if (dict) {
+                dict.onchange(callback);
+            }else{
+                console.error('组件未定义：' + id);
+            }
+        },
+
+        /**
+         * 为组件绑定数据
+         * @param id 组件的ID
+         * @param data 要绑定的数据
+         */
+        setData: function (id, data) {
+            let dict = _cache.get(id);
+            if (dict) {
+                _cache.get(id).setData(data);
+            }else{
+                console.error('组件未定义：' + id);
+            }
         }
+
     };
     exports('dict', component);
 });
