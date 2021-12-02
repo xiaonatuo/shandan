81efcd19de3ba1f01fcd122b040ebc0b3e7b6e91
xiaonatuo <371864209@qq.com>
2021-12-02 09:54:17 +0800

    增加跨域设置

--- shandan-oauth/src/main/java/com/keyware/shandan/config/WebMvcConfiguration.java
+++ shandan-oauth/src/main/java/com/keyware/shandan/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.keyware.shandan.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,18 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index");
         WebMvcConfigurer.super.addViewControllers(registry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                //需要跨域的接口，例如：/openApi/*
+                .addMapping("/**")
+                //允许调用的域，例如：http://172.16.12.156:8888
+                .allowedOrigins("*")
+                //接口调用方式，POST、GET等
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
