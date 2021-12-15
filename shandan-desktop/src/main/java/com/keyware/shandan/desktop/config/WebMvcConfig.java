a04fc77b9528b323df4928176d861e645cc40a46
Fengzhi001 <958110390@qq.com>
2021-12-15 13:56:07 +0800

    桌面管理接口调整

--- shandan-desktop/src/main/java/com/keyware/shandan/desktop/config/WebMvcConfig.java
+++ shandan-desktop/src/main/java/com/keyware/shandan/desktop/config/WebMvcConfig.java
@@ -1,9 +1,5 @@
 package com.keyware.shandan.desktop.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,8 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private ProjectProperties projectProperties;
 
    /* *//**
      * 配置默认跳转首页
@@ -29,15 +23,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
         WebMvcConfigurer.super.addViewControllers(registry);
     }*/
 
-    /**
-     * 文件上传资源虚拟映射
-     *
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPath = projectProperties.getUploadPath();
-        registry.addResourceHandler("/images/**").addResourceLocations("file:///" + uploadPath + "/");
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-    }
+
 }
