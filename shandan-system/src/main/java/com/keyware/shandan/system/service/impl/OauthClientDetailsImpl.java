894a596b67f3aa6b3127b52c7613c8230dc64aaa
xiaonatuo <371864209@qq.com>
2022-02-23 11:54:22 +0800

    客户端秘钥加密护处理

--- shandan-system/src/main/java/com/keyware/shandan/system/service/impl/OauthClientDetailsImpl.java
+++ shandan-system/src/main/java/com/keyware/shandan/system/service/impl/OauthClientDetailsImpl.java
@@ -1,6 +1,9 @@
 package com.keyware.shandan.system.service.impl;
 
+import com.keyware.shandan.common.entity.Result;
 import com.keyware.shandan.common.service.BaseServiceImpl;
+import com.keyware.shandan.common.util.MD5Util;
+import com.keyware.shandan.common.util.StringUtils;
 import com.keyware.shandan.system.entity.OauthClientDetails;
 import com.keyware.shandan.system.mapper.OauthClientDetailsMapper;
 import com.keyware.shandan.system.service.OauthClientDetailsService;
@@ -18,4 +21,14 @@ import org.springframework.stereotype.Service;
 public class OauthClientDetailsImpl extends BaseServiceImpl<OauthClientDetailsMapper, OauthClientDetails, Object>
         implements OauthClientDetailsService {
 
+    @Override
+    public Result<OauthClientDetails> updateOrSave(OauthClientDetails entity) throws Exception {
+        OauthClientDetails old = getById(entity.getId());
+        if (old == null || !old.getClientSecret().equals(entity.getClientSecret())) {
+            if(StringUtils.isNotBlank(entity.getClientSecret())){
+                entity.setClientSecret(MD5Util.getMD5(entity.getClientSecret()));
+            }
+        }
+        return super.updateOrSave(entity);
+    }
 }
